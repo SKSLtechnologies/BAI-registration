40572446f5f5ae777f3426e0d1f2f3acc317d671
smita-samanta <smita.samanta.8@gmail.com>
2019-05-07 15:54:37 +0530

    separate files

--- Anannya/app/routes/bai.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-    const BAIinfo = require('../controllers/bai.controller.js');
-
-    // Create a new member
-    app.post('/new', BAIinfo.create);
-    //View all members
-    app.get('/members', BAIinfo.findAll);
-
-}
--- Anannya/config/database.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url: 'mongodb://ssamanta:3Sz-NSu-fLV-pCU@ds145916.mlab.com:45916/bai-register'
-}
