bcaae67279c9a54c499a456b445d6e7987884031
smita-samanta <smita.samanta.8@gmail.com>
2019-05-16 17:16:36 +0530

    Removed comments

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-//jQuery time
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
@@ -16,22 +15,15 @@ $(".next").click(function () {
     current_fs = $(this).parent();
     next_fs = $(this).parent().next();
 
-    //activate next step on progressbar using the index of next_fs
     $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
 
-    //show the next fieldset
     next_fs.show();
-    //hide the current fieldset with style
     current_fs.animate({
       opacity: 0
     }, {
       step: function (now, mx) {
-        //as the opacity of current_fs reduces to 0 - stored in "now"
-        //1. scale current_fs down to 80%
         scale = 1 - (1 - now) * 0.2;
-        //2. bring next_fs from the right(50%)
         left = (now * 50) + "%";
-        //3. increase opacity of next_fs to 1 as it moves in
         opacity = 1 - now;
         current_fs.css({
           'transform': 'scale(' + scale + ')',
@@ -47,7 +39,6 @@ $(".next").click(function () {
         current_fs.hide();
         animating = false;
       },
-      //this comes from the custom easing plugin
       easing: 'easeInQuad'
     });
 
@@ -72,12 +63,8 @@ $(".previous").click(function () {
     opacity: 0
   }, {
     step: function (now, mx) {
-      //as the opacity of current_fs reduces to 0 - stored in "now"
-      //1. scale previous_fs from 80% to 100%
       scale = 0.8 + (1 - now) * 0.2;
-      //2. take current_fs to the right(50%) - from 0%
       left = ((1 - now) * 50) + "%";
-      //3. increase opacity of previous_fs to 1 as it moves in
       opacity = 1 - now;
       current_fs.css({
         'left': left
@@ -92,41 +79,24 @@ $(".previous").click(function () {
       current_fs.hide();
       animating = false;
     },
-    //this comes from the custom easing plugin
     easing: 'easeInQuad'
   });
 });
 
-// $('.next').click (function(){
-
-// });
-// function validate () {
-//   if (!($("office"))){
-//     alert ("U DA BEST!")
-//   }
-
-// }
 var x, i, j, selElmnt, a, b, c;
-/* Look for any elements with the class "custom-select": */
 x = document.getElementsByClassName("custom-select");
 for (i = 0; i < x.length; i++) {
   selElmnt = x[i].getElementsByTagName("select")[0];
-  /* For each element, create a new DIV that will act as the selected item: */
   a = document.createElement("DIV");
   a.setAttribute("class", "select-selected");
   a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
   x[i].appendChild(a);
-  /* For each element, create a new DIV that will contain the option list: */
   b = document.createElement("DIV");
   b.setAttribute("class", "select-items select-hide");
   for (j = 1; j < selElmnt.length; j++) {
-    /* For each option in the original select element,
-    create a new DIV that will act as an option item: */
     c = document.createElement("DIV");
     c.innerHTML = selElmnt.options[j].innerHTML;
     c.addEventListener("click", function (e) {
-      /* When an item is clicked, update the original select box,
-      and the selected item: */
       var y, i, k, s, h;
       s = this.parentNode.parentNode.getElementsByTagName("select")[0];
       h = this.parentNode.previousSibling;
@@ -148,8 +118,6 @@ for (i = 0; i < x.length; i++) {
   }
   x[i].appendChild(b);
   a.addEventListener("click", function (e) {
-    /* When the select box is clicked, close any other select boxes,
-    and open/close the current select box: */
     e.stopPropagation();
     closeAllSelect(this);
     this.nextSibling.classList.toggle("select-hide");
@@ -158,8 +126,6 @@ for (i = 0; i < x.length; i++) {
 }
 
 function closeAllSelect(elmnt) {
-  /*a function that will close all select boxes in the document,
-  except the current select box:*/
   var x, y, i, arrNo = [];
   x = document.getElementsByClassName("select-items");
   y = document.getElementsByClassName("select-selected");
@@ -176,6 +142,4 @@ function closeAllSelect(elmnt) {
     }
   }
 }
-/*if the user clicks anywhere outside the select box,
-then close all select boxes:*/
 document.addEventListener("click", closeAllSelect);
