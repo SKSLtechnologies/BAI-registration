fa7610a97211ad77186536346fcb622ae79f3e39
smita-samanta <smita.samanta.8@gmail.com>
2019-06-18 22:37:47 +0530

    Materialized the register form

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,37 @@
+$("select.org_type").change(function(){
+  var selectedOption = $(this).children("option:selected").val();
+  if(selectedOption=="Others") {
+    $('#orgSpan').show()
+  }
+  else {
+    $('#orgSpan').hide()
+  }
+  
+});
+$("select.member").change(function(){
+  var selectedOption = $(this).children("option:selected").val();
+  if(selectedOption=="Yes") {
+    $('#bai_code').show()
+  }
+  else {
+    $('#bai_code').hide()
+  }
+  
+});
 var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 
 $(".next").click(function () {
+
   if ($('#city').val() == "" || $('#state').val() == "" || $('#address').val() == "" ||
     $('#contractor-name').val() == "" || $('#pin-code').val() == "" || $('#email').val() == "" ||
     $('#office').val() == "") {
 
-    alert("Please fill out required fields to proceed.")
-  } else {
+    alert("Please fill out required fields to proceed. (*)")
+  }
+
+  else {
     if (animating) return false;
     animating = true;
 
@@ -41,7 +64,6 @@ $(".next").click(function () {
       },
       easing: 'easeInQuad'
     });
-
   }
 
 });
@@ -83,63 +105,57 @@ $(".previous").click(function () {
   });
 });
 
-var x, i, j, selElmnt, a, b, c;
-x = document.getElementsByClassName("custom-select");
-for (i = 0; i < x.length; i++) {
-  selElmnt = x[i].getElementsByTagName("select")[0];
-  a = document.createElement("DIV");
-  a.setAttribute("class", "select-selected");
-  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
-  x[i].appendChild(a);
-  b = document.createElement("DIV");
-  b.setAttribute("class", "select-items select-hide");
-  for (j = 1; j < selElmnt.length; j++) {
-    c = document.createElement("DIV");
-    c.innerHTML = selElmnt.options[j].innerHTML;
-    c.addEventListener("click", function (e) {
-      var y, i, k, s, h;
-      s = this.parentNode.parentNode.getElementsByTagName("select")[0];
-      h = this.parentNode.previousSibling;
-      for (i = 0; i < s.length; i++) {
-        if (s.options[i].innerHTML == this.innerHTML) {
-          s.selectedIndex = i;
-          h.innerHTML = this.innerHTML;
-          y = this.parentNode.getElementsByClassName("same-as-selected");
-          for (k = 0; k < y.length; k++) {
-            y[k].removeAttribute("class");
-          }
-          this.setAttribute("class", "same-as-selected");
-          break;
-        }
-      }
-      h.click();
-    });
-    b.appendChild(c);
-  }
-  x[i].appendChild(b);
-  a.addEventListener("click", function (e) {
-    e.stopPropagation();
-    closeAllSelect(this);
-    this.nextSibling.classList.toggle("select-hide");
-    this.classList.toggle("select-arrow-active");
-  });
-}
+$('select').material_select();
+$(function () {
+  $('input.autocompleteState').autocomplete({
+    data: {
+      "Andhra Pradesh": null,
+      "Arunachal Pradesh": null,
+      "Assam": null,
+      "Bihar": null,
+      "Chhattisgarh": null,
+      "Goa": null,
+      "Gujarat": null,
+      "Haryana": null,
+      "Himachal Pradesh": null,
+      "Jammu and Kashmir": null,
+      "Jharkhand": null,
+      "Karnataka": null,
+      "Kerala": null,
+      "Madhya Pradesh": null,
+      "Maharashtra": null,
+      "Manipur": null,
+      "Meghalaya": null,
+      "Mizoram": null,
+      "Delhi": null,
+      "Nagaland": null,
+      "Odisha": null,
+      "Punjab": null,
+      "Rajasthan": null,
+      "Sikkim": null,
+      "Tamil Nadu": null,
+      "Telangana": null,
+      "Tripura": null,
+      "Uttar Pradesh": null,
+      "Uttarakhand": null,
+      "West Bengal": null,
+    },
 
-function closeAllSelect(elmnt) {
-  var x, y, i, arrNo = [];
-  x = document.getElementsByClassName("select-items");
-  y = document.getElementsByClassName("select-selected");
-  for (i = 0; i < y.length; i++) {
-    if (elmnt == y[i]) {
-      arrNo.push(i)
+  });
+});
+
+// $(input).hover(function () {
+//   $(this).focus();
+// });
+
+$('#msform').validate({
+  errorElement: "div",
+  errorPlacement: function (error, element) {
+    var placement = $(element).data('error');
+    if (placement) {
+      $(placement).append(error)
     } else {
-      y[i].classList.remove("select-arrow-active");
+      error.insertAfter(element);
     }
   }
-  for (i = 0; i < x.length; i++) {
-    if (arrNo.indexOf(i)) {
-      x[i].classList.add("select-hide");
-    }
-  }
-}
-document.addEventListener("click", closeAllSelect);
+});
